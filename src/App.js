0e56d1e61af2d6455bfcfaa773d208df7e4a7dfd
AbdulMoiz8994 <moiza8994@gmail.com>
2021-06-08 01:43:21 +0500

    I have made the nav bar where we add int icon in image and 3 li an one for button

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {Home} from './Components/Home/Home'
+import './App.css';
+
+function App() {
+  return (
+    <div className="main_div">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
