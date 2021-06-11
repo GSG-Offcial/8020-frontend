36b94e70b46eda0d3716b236e928325f8e4a4c65
AbdulMoiz8994 <moiza8994@gmail.com>
2021-06-11 11:10:50 +0500

    Add the four boxes in fourbox file and css also

--- src/Components/DashBoard/DB-Body/FourBox/FourBox.jsx
+++ src/Components/DashBoard/DB-Body/FourBox/FourBox.jsx
@@ -1,34 +1,37 @@
 import React from 'react'
 import './FourBox.css'
+
 export const FourBox = () => {
-    return (
-        <div className="mainbox">
+  return (
+    <div className="mainbox">
+      <div>
+        <p className="boxes" id="box_1">Total Value Locked
+          1000 ETH</p>
+        <p className="amount">$1000k</p>
+      </div>
+      <div>
+        <p className="boxes" id="box_2">
+          Token Supply
+        </p>
+        <p className="amount-2">$1000k</p>
 
-            <div>
-                <p className="boxes" id="box_1">Total Value Locked
-            1000 ETH</p>
-                <p>$1000k</p>
-            </div>
-            <div>
-                <p className="boxes" id="box_2">
-                    Token Supply
-                   </p>
-                <p>$1000k</p>
+      </div>
+      <div className="boxes" id="box_3">
+        <p>Total ETH Deposited
+          since inception  1000k ETH
+        </p>
+        <p className="amount">$1000k</p>
+      </div>
+      <div >
+        <p className="boxes" id="box_4"> Total Dividends
+        </p>
+        Paid 1000k ETH
+        <p className="amount">$1000k</p>
+      </div>
 
-            </div>
-            <div className="boxes" id="box_3">
-                <p>Total ETH Deposited
-                since inception  1000k ETH
-            </p>
-                <p>$1000k</p>
-            </div>
-            <div >
-                <p className="boxes" id="box_4"> Total Dividends
-              </p>
-            Paid 1000k ETH
-            <p>$1000k</p>
-            </div>
-
-        </div>
-    )
+    </div>
+  )
 }
+
+
+
