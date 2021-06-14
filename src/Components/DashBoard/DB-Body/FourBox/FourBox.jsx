6a6c8d97523813a27324635955f2c5ff60cf3ce7
shahzaintariq <shahzaintariq.crpyto@gmail.com>
2021-06-15 01:45:56 +0500

    add contract

--- src/Components/DashBoard/DB-Body/FourBox/FourBox.jsx
+++ src/Components/DashBoard/DB-Body/FourBox/FourBox.jsx
@@ -1,40 +1,70 @@
-import React,{Fragment} from 'react'
-import './FourBox.css'
-
+import React, { Fragment, useState, useEffect } from 'react';
+import './FourBox.css';
+import { getContract, fromWei } from '../../../../utils/index';
+import { abi, address } from '../../../../constants/8020.json';
+import { useWeb3React } from '@web3-react/core';
+import { BigNumber } from '@ethersproject/bignumber';
 
 export const FourBox = () => {
+  const { library, account } = useWeb3React();
+
+  const [contract, setContract] = useState();
+  const [tvl, setTvl] = useState('loading');
+  const [totalSupply, setTotalSupply] = useState('loading');
+  const [eth, setEth] = useState('loading');
+  const [withdrawn, setWithdrawn] = useState('loading');
+
+  useEffect(() => {
+    if (library) {
+      let contractGSG = getContract(abi, address, library, account);
+      setContract(contractGSG);
+    }
+  }, [library]);
+
+  useEffect(async () => {
+    if (contract) {
+      let total_ETH = await contract.totalEthereumBalance();
+      setTvl(+total_ETH.toString());
+
+      let total_supply = await contract.totalSupply();
+      setTotalSupply(fromWei(+total_supply.toString()).toFixed(2));
+
+      let total_dep = await contract.totalDeposited();
+      setEth(fromWei(+total_dep.toString()).toFixed(2));
+
+      let total_withdraw = await contract.totalWithdraw();
+      setWithdrawn(fromWei(+total_withdraw.toString()).toFixed(2));
+    }
+  }, [contract]);
+
   return (
     <Fragment>
-    <div className="mainbox">
-      <div>
-        <p className="boxes" id="box_1">Total Value Locked
-          1000 ETH</p>
-        <p className="amount">$1000k</p>
-      </div>
-      <div>
-        <p className="boxes" id="box_2">
-          Token Supply
-        </p>
-        <p className="amount-2">$1000k</p>
+      <div className="mainbox">
+        <div>
+          <p className="boxes" id="box_1">
+            Total Value Locked {tvl} ETH
+          </p>
+          <p className="amount">$1000k</p>
+        </div>
+        <div>
+          <p className="boxes" id="box_2">
+            Token Supply {totalSupply} GS50
+          </p>
+          <p className="amount-2">$1000k</p>
+        </div>
+        <div className="boxes" id="box_3">
+          <p>Total {eth} ETH Deposited since inception 1000k ETH</p>
+          <p className="amount">$1000k</p>
+        </div>
+        <div>
+          <p className="boxes" id="box_4">
+            {' '}
+            Total {withdrawn} ETH Withdrawn since inception
+          </p>
 
+          <p className="amount">$1000k</p>
+        </div>
       </div>
-      <div className="boxes" id="box_3">
-        <p>Total ETH Deposited
-          since inception  1000k ETH
-        </p>
-        <p className="amount">$1000k</p>
-      </div>
-      <div >
-        <p className="boxes" id="box_4"> Total Dividends
-        </p>
-        Paid 1000k ETH
-        <p className="amount">$1000k</p>
-      </div>
-
-    </div>
     </Fragment>
-  )
-}
-
-
-
+  );
+};
