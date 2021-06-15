c29cf834fb35f476b55e21d799367b114fa7d5da
shahzaintariq <shahzaintariq.crpyto@gmail.com>
2021-06-15 16:28:14 +0500

    fix css

--- src/Components/DashBoard/DB-Body/TwoBox/TwoBox.jsx
+++ src/Components/DashBoard/DB-Body/TwoBox/TwoBox.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
-import './TwoBox.module.css'
+import React from 'react';
+import './TwoBox.css';
 export const TwoBox = () => {
-    return (
-             
-        <div className="two-box">
-            <div>
-               <ul className="twobox-ul">
-                   <li className="button button-1">Buy</li>
-                   <li className="button button-2">Sell</li>
-               </ul>
-          
-                <ul className="second-ul">
-                    <li className="links link-1">Enter ETN</li>
-                    <li className="links link-2">Max</li>
-                </ul>
-           
-                <p className="box-para">You will give 1000 Token</p>
-                <form>
-                <input type="submit" value="Buy" className="buy-button" />
-              
-                </form>
-            </div>
-            <div>
-         <h1>Hello</h1>
-        </div>
-        </div>
-     
-    )
-}
+  return (
+    <div className="two-box">
+      <div>
+        <ul className="twobox-ul">
+          <li className="button button-1">Buy</li>
+          <li className="button button-2">Sell</li>
+        </ul>
+
+        <ul className="second-ul">
+          <li className="links link-1">Enter ETN</li>
+          <li className="links link-2">Max</li>
+        </ul>
+
+        <p className="box-para">You will give 1000 Token</p>
+        <form>
+          <input type="submit" value="Buy" className="buy-button" />
+        </form>
+      </div>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+};
--- src/Components/Lottery/Header/Header.jsx
+++ src/Components/Lottery/Header/Header.jsx
@@ -1,33 +1,39 @@
-import React from 'react'
-import './Header.module.css'
-
+import React from 'react';
+import './Header.css';
 
 export const Header = () => {
-    return (
-        <div className="contianer-home" id="lotter-maindiv">
+  return (
+    <div className="contianer-home" id="lotter-maindiv">
+      <div>
+        <h1>
+          <span className="badge header-heading">DashBoard</span>
+        </h1>
+      </div>
 
-            <div>
-                <h1><span className="badge header-heading">DashBoard</span></h1>
-            </div>
-
-            <div>
-                <ul className="nav justify-content-end" id="home-ul">
-                    <li className="nav-item" id="lottery-li">
-                        <a className="nav-link active" aria-current="page" href="#/">Dashboard</a>
-                    </li>
-                    <li className="nav-item" id="lottery-li2">
-                        <a className="nav-link active " aria-current="page" href="#/">Your spons ID: 0x02354309</a>
-                    </li>
-                    <li className="nav-item" id="lottery-li3">
-                        <a className="nav-link active" aria-current="page" href="#/">Copy Ref. link</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <form class="d-flex">
-                    <input type="submit" value="Connect" className="lottery-btn" />
-                </form>
-            </div>
-        </div>
-    )
-}
+      <div>
+        <ul className="nav justify-content-end" id="home-ul">
+          <li className="nav-item" id="lottery-li">
+            <a className="nav-link active" aria-current="page" href="#/">
+              Dashboard
+            </a>
+          </li>
+          <li className="nav-item" id="lottery-li2">
+            <a className="nav-link active " aria-current="page" href="#/">
+              Your spons ID: 0x02354309
+            </a>
+          </li>
+          <li className="nav-item" id="lottery-li3">
+            <a className="nav-link active" aria-current="page" href="#/">
+              Copy Ref. link
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <form class="d-flex">
+          <input type="submit" value="Connect" className="lottery-btn" />
+        </form>
+      </div>
+    </div>
+  );
+};
