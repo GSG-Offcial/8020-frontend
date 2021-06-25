cdfef557494fca1a4b7d7d5d051a7fe3d3fd933a
AbdulMoiz8994 <moiza8994@gmail.com>
2021-06-25 22:43:05 +0500

    add the animation in submit and Descalmier comp button

--- src/Components/Home/HomeDesc/HomeDesc.jsx
+++ src/Components/Home/HomeDesc/HomeDesc.jsx
@@ -23,7 +23,7 @@ export const HomeDesc = () => {
                         </p>
                     </div>
                     <div className={`col-2 mx-auto d-flex justify-content-center text-center fw-bolder  my-4 ${style.HomeDescButton}`}>
-                 <button type="button" className={`btn text-white  px-5 py-3`}>DESCLAMIER</button>
+                 <button type="button" className={`btn text-white  px-5 py-3 ${style.HomeDesc_ORIGButton}`}>DESCLAMIER</button>
                  </div>
                 </div>
                 </div>
