40052fbf77053e54fc5bca55233b22cf85d22125
shahzaintariq <shahzaintariq.crpyto@gmail.com>
2021-06-29 18:09:39 +0500

    complete last number

--- src/Components/Lottery/LotBottomTwoBox/BottomTwoBox.jsx
+++ src/Components/Lottery/LotBottomTwoBox/BottomTwoBox.jsx
@@ -1,21 +1,76 @@
-import React from 'react'
-import styles from './BottomTwoBox.module.css' 
-export const BottomTwoBox = () => {
-    return (
-        <div className={`${styles.main_BottomTwoBox} row mb-3`}>
-            <div className="col-lg-6 col-md-12 mb-4">
-            <div className={styles.BottomTwoBox_nesteddiv}>
-              <p>Last Pick 3 Numbers</p>
-              <p className={styles.nub_bottomTwoBox}>9 7 5</p>
-            </div>
-            </div>
+import React, { useState } from 'react';
+import styles from './BottomTwoBox.module.css';
+import { useContract } from '../../../Hooks/lottery';
+import { useEffect } from 'react';
 
-            <div className="col-lg-6 col-md-12">
-                <div className={styles.BottomTwoBox_nesteddiv}>
-             <p>Last Pick 4 Numbers</p>
-               <p className={styles.nub_bottomTwoBox}>8 2 2 9</p>
-                </div>
-            </div>
+export const BottomTwoBox = () => {
+  const contract = useContract();
+
+  const [pick3Number, setPick3Number] = useState('0 0 0');
+  const [pick4Number, setPick4Number] = useState('0 0 0 0');
+
+  function formatValueOfPick3(lastNumber) {
+    if (lastNumber < 10) {
+      return `0 0 ${lastNumber}`;
+    }
+    if (lastNumber > 10 && lastNumber < 100) {
+      return '0 ' + lastNumber.split('').join(' ');
+    } else {
+      return lastNumber.split('').join(' ');
+    }
+  }
+
+  function formatValueOfPick4(lastNumber) {
+    if (lastNumber < 10) {
+      return '0 0 0 ' + lastNumber;
+    }
+    if (lastNumber > 10 && lastNumber < 100) {
+      return '0 0 ' + lastNumber.split('').join(' ');
+    } else if (lastNumber > 100 && lastNumber < 1000) {
+      return '0 ' + lastNumber.split('').join(' ');
+    } else {
+      return lastNumber.split('').join(' ');
+    }
+  }
+
+  useEffect(async () => {
+    if (!!contract) {
+      let pick3Numbers = [];
+      let pick4Numbers = [];
+      const pick3Events = await contract.queryFilter(contract.filters.Random());
+      pick3Events.map(({ args }) => {
+        if (Number(String(args.pick)) == 3) {
+          pick3Numbers.push(String(args.number));
+        } else if (Number(String(args.pick)) == 4) {
+          pick4Numbers.push(String(args.number));
+        }
+      });
+      setPick3Number(pick3Numbers[pick3Numbers.length - 1]);
+      setPick4Number(pick4Numbers[pick4Numbers.length - 1]);
+    }
+  }, [contract]);
+
+  console.log('pick3 Number', pick3Number);
+
+  return (
+    <div className={`${styles.main_BottomTwoBox} row mb-3`}>
+      <div className="col-lg-6 col-md-12 mb-4">
+        <div className={styles.BottomTwoBox_nesteddiv}>
+          <p>Last Pick 3 Numbers</p>
+          <p className={styles.nub_bottomTwoBox}>
+            {formatValueOfPick3(pick3Number)}
+          </p>
         </div>
-    )
-}
+      </div>
+
+      <div className="col-lg-6 col-md-12">
+        <div className={styles.BottomTwoBox_nesteddiv}>
+          <p>Last Pick 4 Numbers</p>
+          <p className={styles.nub_bottomTwoBox}>
+            {formatValueOfPick4(pick4Number)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
