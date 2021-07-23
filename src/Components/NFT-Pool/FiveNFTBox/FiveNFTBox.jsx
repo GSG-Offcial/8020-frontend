499aaf366919fa9c08fd353cbe3dd718f7073400
AbdulMoiz8994 <moiza8994@gmail.com>
2021-07-24 00:04:02 +0500

    add the five box of nft and some mdeia query in mintoken and fivenftbox comp

--- src/Components/NFT-Pool/FiveNFTBox/FiveNFTBox.jsx
+++ src/Components/NFT-Pool/FiveNFTBox/FiveNFTBox.jsx
@@ -5,6 +5,51 @@ export const FiveNFTBox = () => {
     return (
         <div className={` ${styles.FiveNFTBox_mainDiv}`}>
             <h1>Stake your NFT</h1>
+            {/* five Box */}
+            <div className={`row justify-content-center d-flex ${styles.nested_FiveBox}`}>
+                <div className={`col-lg-4 col-md-12`}>
+                    <div className={` ${styles.firstBox}`}>
+                        <p className={`mt-3 ${styles.para_statis_nft}`}>
+                            Total NFT Staked
+                        </p>
+                        <p className={` ${styles.amount_nft}`}>100 NFT ($100)</p>
+                    </div>
+                </div>
+
+                <div className={` col-lg-4 col-md-12`}>
+                    <div className={` ${styles.firstBox}`}>
+                        <p className={`mt-3 ${styles.para_statis_nft}`}>
+                            Total NFT Staked
+                        </p>
+                        <p className={` ${styles.amount_nft}`}>100 NFT ($100)</p>
+                    </div>
+                </div>
+                <div className={`col-lg-4 col-md-12`}>
+                    <div className={` ${styles.firstBox}`}>
+                        <p className={`mt-3 ${styles.para_statis_nft}`}>
+                            Total NFT Staked
+                        </p>
+                        <p className={` ${styles.amount_nft}`}>100 NFT ($100)</p>
+                    </div>
+                </div>
+                <div className={`col-lg-4 col-md-12`}>
+                    <div className={` ${styles.firstBox}`}>
+                        <p className={`mt-3 ${styles.para_statis_nft}`}>
+                            Total NFT Staked
+                        </p>
+                        <p className={` ${styles.amount_nft}`}>100 NFT ($100)</p>
+                    </div>
+                </div>
+
+                <div className={`col-lg-4 col-md-12`}>
+                    <div className={` ${styles.firstBox}`}>
+                        <p className={`mt-3 ${styles.para_statis_nft}`}>
+                            Total NFT Staked
+                        </p>
+                        <p className={` ${styles.amount_nft}`}>100 NFT ($100)</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
