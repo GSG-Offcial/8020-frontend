a4033844964b3c313e37325ec77544399a16c76e
shahzaintariq <shahzaintariq.crpyto@gmail.com>
2021-07-27 20:07:55 +0500

    changes in dashborad

--- src/Components/DashBoard/DashBoardNavbar/DashBoardNavbar.jsx
+++ src/Components/DashBoard/DashBoardNavbar/DashBoardNavbar.jsx
@@ -202,8 +202,8 @@ export const DashBoardNavbar = () => {
                 {account === undefined
                   ? 'Connect'
                   : account === null
-                    ? 'None'
-                    : `${account.substring(0, 6)}...${account.substring(
+                  ? 'None'
+                  : `${account.substring(0, 6)}...${account.substring(
                       account.length - 4
                     )}`}
               </a>
@@ -222,8 +222,8 @@ export const DashBoardNavbar = () => {
                 {account === undefined
                   ? 'Connect'
                   : account === null
-                    ? 'None'
-                    : `${account.substring(0, 6)}...${account.substring(
+                  ? 'None'
+                  : `${account.substring(0, 6)}...${account.substring(
                       account.length - 4
                     )}`}
               </a>
@@ -233,12 +233,12 @@ export const DashBoardNavbar = () => {
       </nav>
 
       <div className="DashBoard_comp">
-        <SideBar className="sidebarBox"/>
-      <div >
-        <FourBox price={ethPrice} GS50Price={tokenPrice} />
-        <TwoBox refAddress={refAddress} />
-        <BottomFourBox/>
-        <BottomTwoBox price={ethPrice} />
+        <SideBar className="sidebarBox" />
+        <div>
+          <FourBox price={ethPrice} GS50Price={tokenPrice} />
+          <TwoBox refAddress={refAddress} />
+          <BottomFourBox price={ethPrice} GS50Price={tokenPrice} />
+          <BottomTwoBox price={ethPrice} />
         </div>
       </div>
       <Desclaimer />
