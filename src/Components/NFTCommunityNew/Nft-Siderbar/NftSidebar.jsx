a176fcd5bf48d73cd9203f1f6293cb7a56522919
AbdulMoiz8994 <moiza8994@gmail.com>
2021-11-18 21:36:36 +0500

    add the dropdow in nftsidebarComp and nftsidebarNew

--- src/Components/NFTCommunityNew/Nft-Siderbar/NftSidebar.jsx
+++ src/Components/NFTCommunityNew/Nft-Siderbar/NftSidebar.jsx
@@ -86,6 +86,20 @@ export const NftSidebar = () => {
                 </span>{' '}
               </a>
             </Link>
+            <div class="dropdown">
+  <button className={`btn btn-secondary  ${styles.button_dropDown_nftSidebarNew}`} type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+  <span>
+                <i className={`fas fa-dice-d20 ${styles.ion_dropDownNew}`}></i>
+              </span>{' '}    
+              NFT Launched Paid
+  </button>
+  <ul className={`dropdown-menu dropdown-menu-dark ${styles.items_dropDown_NftSidebarNew}`} aria-labelledby="dropdownMenuButton2">
+    <li><a className={`${styles.dropdown_item_nftsidebarNew}`} href="#">ART</a></li>
+    <li><a className={`${styles.dropdown_item_nftsidebarNew}`} href="#">MUSIC</a></li>
+    <li><a className={`${styles.dropdown_item_nftsidebarNew}`} href="#">SPORTS</a></li>
+    <li><a className={`${styles.dropdown_item_nftsidebarNew}`} href="#">UTILITY</a></li>
+  </ul>
+</div>
           </li>
         </ul>
       </div>
