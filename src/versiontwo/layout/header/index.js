8e5d4a655ad64a21ec7566cffd40d0676b0ea2b4
AbdulMoiz8994 <moiza8994@gmail.com>
2022-07-12 12:22:20 +0500

    add the changes and full content:

--- src/versiontwo/layout/header/index.js
+++ src/versiontwo/layout/header/index.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import React from "react";
 import style from "./index.module.css";
 import logo from "../../assets/Icons/Group900.svg";
 
 export const Header = () => {
   return (
     <div className={style.updated_header}>
-     <div className={style.nested_header}>
-           
-           <div className={style.img_header}>
-             <img src={logo} alt="8020-logo" />
-           </div>
-           <div className={style.boxes_header}>
-             <div className={style.nested_box}>
-                <p>Total Deposited - 750 BNB</p>
-                <p>Total WithDrawin - 630 BNB</p>
-            </div>  
-            <div className={style.nested_box}>
-                <p>Total Deposited - 750 BNB</p>
-                <p>Total WithDrawin - 630 BNB</p>
-
-            </div>
-            </div> 
-        </div>   
+      <div className={style.nested_header}>
+        <div className={style.img_header}>
+          <img src={logo} alt="8020-logo" />
+        </div>
+        <div className={style.boxes_header}>
+          <div className={style.nested_box}>
+            <p>Total Deposited - 750 BNB</p>
+            <p>Total WithDrawin - 630 BNB</p>
+          </div>
+          <div className={style.nested_box}>
+            <p>Access Pool - 2 BNB</p>
+            <p>Community Pool - 1.4 BNB</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/versiontwo/modules/stakingpool/index.js
+++ src/versiontwo/modules/stakingpool/index.js
@@ -1,26 +1,153 @@
 import React from "react";
 import styles from "./index.module.css";
 import { Header, Sidebar } from "../../layout";
+import { Btn } from "../../components";
+import image from "../../assets/images/minToken2.gif";
 
 export const StakingPool = () => {
   return (
     <div className={styles.main_stakingpool}>
       <div className={styles.nested_satkingpool}>
         <Header />
-      
-      <div className={styles.content_stakingpool}>
-        {/* sidebar div */}
-        <div>
-          <Sidebar />
+
+        <div className={styles.content_stakingpool}>
+          {/* sidebar div */}
+          <div>
+            <Sidebar />
+          </div>
+          <div className={styles.stakingpool_data}>
+            <div className={styles.nested_stakingpool}>
+              <div className={styles.network_tab}>
+                <div className={styles.network_heading}>Network - Binance</div>
+                <div className={styles.network_heading}>
+                  Wallet - 0x123***555
+                </div>
+              </div>
+              <h5>Access Key Staking pool</h5>
+
+              <div className={styles.access_key}>
+                <div className={styles.access_content}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      gap: "10px",
+                    }}
+                  >
+                    <div className={styles.child_status}>
+                      <p>Tvl</p>
+                      <p>100</p>
+                      <p>Bnb</p>
+                    </div>
+                    <div className={styles.child_status}>
+                      <p>Tvl</p>
+                      <p>100</p>
+                      <p>Bnb</p>
+                    </div>
+                    <div className={styles.child_status}>
+                      <p>Tvl</p>
+                      <p>100</p>
+                      <p>Bnb</p>
+                    </div>
+                  </div>
+                  <div className={styles.status_staking}>
+                    <p>User Reward:</p>
+                    <p className={styles.amount}>1.1 BNB</p>
+                  </div>
+                </div>
+
+                <div className={styles.accessbtn}>
+                  <Btn value={"STAKE/UNSTAKE"} />
+                  <Btn value={"CLAIM REWARD"} />
+                </div>
+              </div>
+
+              {/* line */}
+              <hr />
+
+              <div className={styles.access_key}>
+                <div className={styles.access_content}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      gap: "10px",
+                    }}
+                  >
+                    <div className={styles.child_status}>
+                      <p>Tvl</p>
+                      <p>100</p>
+                      <p>Bnb</p>
+                    </div>
+                    <div className={styles.child_status}>
+                      <p>Tvl</p>
+                      <p>100</p>
+                      <p>Bnb</p>
+                    </div>
+                    <div className={styles.child_status}>
+                      <p>Tvl</p>
+                      <p>100</p>
+                      <p>Bnb</p>
+                    </div>
+                  </div>
+                  <div className={styles.status_staking}>
+                    <p>User Reward:</p>
+                    <p className={styles.amount}>1.1 BNB</p>
+                  </div>
+                </div>
+
+                <div className={styles.accessbtn}>
+                  <Btn value={"STAKE/UNSTAKE"} />
+                  <Btn value={"CLAIM REWARD"} />
+                </div>
+              </div>
+              {/* <hr /> */}
+              <div className={styles.wallet_balance}>
+                <p>Wallet balance</p>
+                <p>1,000 Gs50</p>
+                <p>10 Bnb</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.side_data}>
+            <div className={styles.netsed_sideData}>
+              <h3>Access Key NFT</h3>
+
+              <div className={styles.side_content}>
+                <div style={{width: "100%" }}>
+                  <img src={image} alt="gif" width={"100%"} height={"210px"} />
+                  <div className={styles.accessbtn}>
+                    <Btn value={"STAKE/UNSTAKE"} />
+                    <Btn value={"CLAIM REWARD"} />
+                  </div>
+                </div>
+               <div className={styles.description}>
+                 <p>NFT DEscription</p>         
+                 <p>Price GS50/BNB</p>
+                </div>    
+              </div>
+              <hr />
+              <div className={styles.side_content}>
+                <div style={{width: "100%" }}>
+                  <img src={image} alt="gif" width={"100%"} height={"210px"} />
+                  <div className={styles.accessbtn}>
+                    <Btn value={"STAKE/UNSTAKE"} />
+                    <Btn value={"CLAIM REWARD"} />
+                  </div>
+                </div>
+               <div className={styles.description}>
+                 <p>NFT DEscription</p>         
+                 <p>Price GS50/BNB</p>
+                </div>    
+              </div>
+            </div>
+          </div>
+          <div className={styles.advertisment_data}>
+            <p style={{ color: "gray" }}>Advertisment space</p>
+          </div>
         </div>
-    <div className={styles.stakingpool_data}>
-
-    </div>
-    <div className={styles.side_data}>
-
-    </div>
-
-      </div>
       </div>
     </div>
   );
